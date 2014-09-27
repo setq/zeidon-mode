edd3f748b0135a9078d052071153424d82434d56
Justin Schmitz <justin.schmitz.21@gmail.com>
2014-09-27 07:34:48 -0400

    Added: new copy method, get-parent-entity

--- zeidon.el
+++ zeidon.el
@@ -5,21 +5,49 @@
 
 (defun create-lod-view-buffer(sName)
   (setq sLod (concat "*LOD " sName " View*"))
-  (message "Buffer window: %s" sLod)
   (if (get-buffer sLod)
       (kill-buffer sLod))
   (generate-new-buffer sLod)
   (change-to-viewer sLod)
-  (goto-char (point-min))
-  (insert "|--------------------------------------------|\n")
-  (insert "| Logical Object Definition Viewer for Emacs |\n")
-  (insert "|--------------------------------------------|\n")
+  (insert "--------------------------------------------------------------------------\n")
+  (insert " Logical Object Definition Viewer for Emacs\n")
+  (insert "--------------------------------------------------------------------------\n")
+  (insert " Viewing LOD:-> ")
+  (insert sName)(insert ".LOD\n")
+  (insert "--------------------------------------------------------------------------\n\n")
+  (insert "---\n\n")
+  (load-lod sName)
+  (message "Buffer window: %s" sLod)
+  (build-lod-view-buffer sName sLod)
  )
+
+(defun load-lod (sName)
+  (setq szPath (concat "~/Projects/emacs/zeidon/src/" sName ".LOD"))
+  (find-file-noselect szPath)
+)
+
+(defun build-lod-view-buffer(sName sLod)
+  (setq szReadBuf (concat sName ".LOD"))
+  (change-to-viewer szReadBuf)
+  (search-forward "eLOD_EntityParent")
+  (search-forward "aName")
+  (skip-chars-forward " ")
+  (setq szT (extract-word))
+  (change-to-viewer sLod)
+  (goto-char (point-max))
+  (insert "Parent Entity: ") (insert szT)
+  (newline)
+)
+
+(defun get-parent-entity (sReadBuf)
+  
+)
 
 (defun extract-word ()
   (let ((begin (point)))
     (forward-word)
-    (kill-ring-save begin (point)))
+    (buffer-substring-no-properties begin (point)))
+    ;(kill-ring-save begin (point)))
 )
 
 (defun get-lod-for-viewer ()
@@ -29,12 +57,9 @@
   (yank)
 )
 
-(defun change-to-viewer (sLod)
-  (switch-to-buffer-other-window sLod))
-
-
-(defun build-lod-name (&optional arg)
-  (interactive "sLod Name: " arg)
-  (setq sName (concat "*" arg "*")))
+(defun change-to-viewer (sLodBuffer)
+  (switch-to-buffer-other-window sLodBuffer)
+  (goto-char (point-min))
+)
 
 
